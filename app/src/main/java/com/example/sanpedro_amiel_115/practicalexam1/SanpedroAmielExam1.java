71b36df4ca65ab7355720bf9de5564feb27bc4b1
unknown <SAN PEDRO_AMIEL_115@IICSGENERAL.com>
2018-10-15 16:45:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sanpedro_amiel_115/practicalexam1/SanpedroAmielExam1.java
@@ -0,0 +1,13 @@
+package com.example.sanpedro_amiel_115.practicalexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SanpedroAmielExam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sanpedro_amiel_exam1);
+    }
+}
